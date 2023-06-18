f32508698727836dcf7886a2ec0342c7a056efcc
Taufiq Zarif <taufiqzarif90@gmail.com>
2023-06-18 17:45:36 +0800

    Initial commit

---
+++ src/main/java/net/alyph/partychatmod/PartyChatMod.java
@@ -0,0 +1,20 @@
+package net.alyph.partychatmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PartyChatMod implements ModInitializer {
+	public static final String MOD_ID = "partychatmod";
+    public static final Logger LOGGER = LoggerFactory.getLogger("partychatmod");
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/alyph/partychatmod/PartyChatModClient.java
@@ -0,0 +1,10 @@
+package net.alyph.partychatmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PartyChatModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
